4f0d4c0a7fe4faaa739b8d5d031bfbdde0be6304
Mohamed Amine Nadim <nadim.mohamedamine.pro@gmail.com>
2024-04-18 09:48:14 +0200

    feature/Added messager

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -8,7 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        Messager messager = new Messager("This is the feature #002");
         System.out.println( "CI CD update - release 1.1" );
         System.out.println( "Package update - release 1.1.1" );
+        System.out.println(messager.getMessage());
     }
 }
